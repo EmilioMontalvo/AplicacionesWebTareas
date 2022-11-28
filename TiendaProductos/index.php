14b6b4f0b2aac0c835a93d361d179bfba6ba6de1
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2022-11-28 12:17:48 -0500

    inicio

---
+++ TiendaProductos/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+<html>
+    <head>
+    </head>
+    <body>
+    <div id="container">
+        <form method="POST" action="panelPrincipal.php">
+            <fieldset>
+                <h1>LOGIN</h1>
+                Usuario:<br/> <input type="text" name="nombre"/><br/><br/>
+                Clave:<br/> <input type="password" name="clave"/><br/>
+                <input type="checkbox"><label class="check" for="checkbox">Recordarme</label><br/>
+                <input type="submit" value="Enviar"/>
+            </fieldset>
+        </form>
+    </div>
+    </body>
+
+    
+</html>
---
+++ TiendaProductos/panelPrincipal.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+    
+            <fieldset>
+                <h1>Panel Principal</h1>
+                <h3>Bienvenido Usuario: <?php echo $_POST["nombre"];?></h3>
+                
+                <input type="submit" value="Enviar"/>
+                
+            </fieldset>
+        
+    </body>
+</html>
